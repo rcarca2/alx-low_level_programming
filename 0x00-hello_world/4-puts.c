43c0350193d93e7fd532209e9f562eee5517a61c
rcarca2 <yassineelfajri639@gmail.com>
2023-09-07 15:10:38 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main(void)
 *Write a C program that prints exactly 
 *"Programming is like building a multilingual puzzle
 *followed by a new line.
-*\
+*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
