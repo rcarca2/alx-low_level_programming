81901e35c0ee75e8f0f2eb1308326950ecafe5d3
rcarca2 <yassineelfajri639@gmail.com>
2023-09-08 12:01:23 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,8 +15,8 @@ int main(void)
 if (n > 5)
 printf(n ,"and is greater than 5\n");
 if (n == 0)
-printf(n ,"and is 0\n")
+printf(n ,"and is 0\n");
 if (n > 0 && n < 6)
-printf(n ,"and is less than 6 and not 0\n")
+printf(n ,"and is less than 6 and not 0\n");
 	return (0);
 }
