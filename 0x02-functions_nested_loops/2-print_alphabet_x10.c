b6ca7366a98700bae92f727c29a71d2fab3f548f
rcarca2 <yassineelfajri639@gmail.com>
2023-09-12 15:50:49 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet_x10(void);
+{
+int letter;
+for (int i ; i =< 10 ; i++){
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar (10);
+}
+}
