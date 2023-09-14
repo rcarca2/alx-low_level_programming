edd8c4b6775d12ea0cf7d48238588edf4a162d56
rcarca2 <yassineelfajri639@gmail.com>
2023-09-14 15:38:14 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+
+void print_numbers(void)
+{
+int a;
+for (number=1 ; number<=9 ; number++)
+_putchar(number);
+_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@
 *Desc:header file containing functions declaration
 */
 
+void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
