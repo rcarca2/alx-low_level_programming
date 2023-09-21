4c58ec859253ec1e733dcab9078b1c20ff39ba5b
rcarca2 <yassineelfajri639@gmail.com>
2023-09-21 14:10:56 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int a, b;
+a = 0;
+while (dest[a])
+	a++;
+for (b = 0; src[b]; b++)
+	dest[a++] = src[b];
+return (dest);
