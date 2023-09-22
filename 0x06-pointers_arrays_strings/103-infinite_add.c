4407cbae50ba6e0d36b7c84595bf8a48a9ada9e7
rcarca2 <yassineelfajri639@gmail.com>
2023-09-21 17:30:39 -0700

    11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,61 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * infinite_add - add 2 strings.
- * @n1: string1.
- * @n2: string2.
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
-	
-	while (n1[a_len] != '\0')
-		a_len++;
-	while (n2[b_len] != '\0')
-		b_len++;
-	if (a_len > b_len)
-		biggest = a_len;
-	else
-		biggest = b_len;
-	if ((biggest + 1) >= size_r)
-		return (0);
-	r[biggest + 1] = '\0';
-	
-	while (biggest >= 0)
-	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
-		if (a_len > 0 && b_len > 0)
-			sum = a + b + carry;
-		else if (a_len < 0 && b_len > 0)
-			sum = b + carry;
-		else if (a_len > 0 && b_len < 0)
-			sum = a + carry;
-		else
-			sum = carry;
-		
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
-		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[biggest] = sum;
-		a_len--;
-		b_len--;
-		biggest--;
-	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+#include <string.h>
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int carry = 0;
+    
+    if (len1 + 1 > size_r || len2 + 1 > size_r) {
+        // Result won't fit in the buffer
+        return 0;
+    }
+
+    int i = len1 - 1;
+    int j = len2 - 1;
+    int k = 0;
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+        int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+        
+        int sum = digit1 + digit2 + carry;
+        carry = sum / 10;
+        r[k] = (sum % 10) + '0';
+        
+        i--;
+        j--;
+        k++;
+    }
+
+    r[k] = '\0';
+
+    // Reverse the result string
+    int left = 0;
+    int right = k - 1;
+    while (left < right) {
+        char temp = r[left];
+        r[left] = r[right];
+        r[right] = temp;
+        left++;
+        right--;
+    }
+
+    return r;
 }
+
+int main() {
+    char n1[] = "123456789012345678901234567890";
+    char n2[] = "987654321098765432109876543210";
+    char result[50];  // Adjust the buffer size as needed
+
+    char *sum = infinite_add(n1, n2, result, sizeof(result));
+    if (sum) {
+        printf("Sum: %s\n", sum);
+    } else {
+        printf("Result cannot fit in the buffer.\n");
+    }
+
+    return 0;
+}
+
