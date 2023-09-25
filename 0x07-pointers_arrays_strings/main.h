abeb79c08d91fd15ef22f4b4627705172a534d49
rcarca2 <yassineelfajri639@gmail.com>
2023-09-25 12:48:40 -0700

    1

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+*File:main.h
+*Author:Pericles Adjovi
+*Desc:header file containing functions declaration
+*/
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif /* MAIN_H */
