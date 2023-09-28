5bcc499d7a631c0675b061af3bc727e3f3e38f81
rcarca2 <yassineelfajri639@gmail.com>
2023-09-28 14:27:27 -0700

    12

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+*File:main.h
+*Author:Pericles Adjovi
+*Desc:header file containing functions declaration
+*/
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+
+
+
+
+#endif /* MAIN_H */
