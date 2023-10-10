ab2f7ca01a5b9ea759e33f00ee3137fd208af7c7
rcarca2 <yassineelfajri639@gmail.com>
2023-10-10 10:43:21 -0700

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
